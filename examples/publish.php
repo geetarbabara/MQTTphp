fd02696813c637cfd669b97bf79fd45bdd2d31c4
geetarbabara <41662458+geetarbabara@users.noreply.github.com>
2018-07-25 18:48:43 +0700

    Update publish.php

--- examples/publish.php
+++ examples/publish.php
@@ -2,10 +2,10 @@
 
 require("../phpMQTT.php");
 
-$server = "mqtt.example.com";     // change if necessary
-$port = 1883;                     // change if necessary
-$username = "";                   // set your username
-$password = "";                   // set your password
+$server = "m10.cloudmqtt.com";     // change if necessary
+$port = 18468;                     // change if necessary
+$username = "fqepuwcl";                   // set your username
+$password = "B57JqOt6rFfu";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $mqtt = new phpMQTT($server, $port, $client_id);
